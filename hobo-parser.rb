5a16dfe1de28511b58d16fae2b36f5914c66f29b
Sven Bohm <bohms@msu.edu>
2013-10-01 15:43:11 -0400

    basic parsing works still need to work out the control file

--- hobo-parser.rb
+++ hobo-parser.rb
@@ -1,22 +1,46 @@
 require 'time'
+require 'chronic'
 require 'csv'
+require 'yaml'
 
 class HoboParser
   attr_accessor :control_file, :cut_list
 
   def initialize(control='')
-    self.control_file = control
-  end
-
-  def control_file=(control)
-   cut_list = [
-     {id: 1, plot: 'G1', from: Time.parse('2013-9-25 9:30'), to: Time.parse('2013-9-25 10:30')}
-   ]
+   @cut_list = YAML.load(File.read('./control.yaml'))
+   @output = {}
   end
 
   def parse(file)
-    CSV.foreach(file) do |row|
-      p row
+    internal = false
+    lines = CSV.read(file, encoding: 'WINDOWS-1252:UTF-8')
+    title = lines.shift
+    lines.shift
+    id = title[0].split()[2]
+
+    lines.each do |row|
+      sampled_at = Chronic.parse(row[1])
+      list = @cut_list.first {|x| x[:id] == id }
+      if sampled_at > list[:from] &&
+        sampled_at < list[:to]
+        unless @output["#{list[:id]}-#{sampled_at}"]
+          @output["#{list[:id]}-#{sampled_at}"] = [list[:id],list[:plot],sampled_at]
+        end
+        if list[:internal]
+          @output["#{list[:id]}-#{sampled_at}"][3] = row[2]
+        else
+          @output["#{list[:id]}-#{sampled_at}"][4] = row[2]
+        end
+        @output["#{list[:id]}-#{sampled_at}"][5] = row[3]
+      end
+    end
+  end
+
+  def write
+    CSV do |csv|
+      @output.each do |row|
+        csv << row[1]
+      end
     end
   end
 end
--- hobo.rb
+++ hobo.rb
@@ -2,12 +2,19 @@ require 'main'
 require_relative 'hobo-parser'
 
 Main {
-  # argument 'control'
+  description 'A program to collect logger data only for the times when the loggers were in use'
+
+  option('control') {
+    arity 1
+    argument :optional
+    description 'a control file specifying which times to save. Default "control.yaml"'
+  }
 
   def run
-    parser = Parser.new(params['control'])
+    parser = HoboParser.new()
     Dir.glob('*.csv').each do |file|
       parser.parse(file)
     end
+    parser.write
   end
 }
